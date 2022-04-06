482ab5c269b87d2a082a77c99ec62863a3797dfa
dallinl <dladle99@gmail.com>
2022-04-05 22:51:26 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using IntexApril.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntexApril.Controllers
+{
+    public class HomeController : Controller
+    {
+        private InferenceSession _session;
+        //private AccidentInfo _context { get; set; }
+
+        public HomeController(InferenceSession session)
+        {
+            _session = session;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        
+        public IActionResult SummaryPage()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult PredictionCalculator()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult PredictionCalculator(AccidentInfo data)
+        {
+            var result = _session.Run(new List<NamedOnnxValue>
+            {
+                NamedOnnxValue.CreateFromTensor("float_input", data.AsTensor())
+            });
+            Tensor<float> score = result.First().AsTensor<float>();
+            var prediction = new CrashPrediction { CrashSeverity = score.First()};
+            //result.Dispose();
+            return View("PredictionResult", prediction);
+        }
+
+    }
+}
---
+++ Models/AccidentInfo.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.ML.OnnxRuntime.Tensors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntexApril.Models
+{
+    public class AccidentInfo
+    {
+        public float MILE_POINT { get; set; }
+        public float WORK_ZONE_RELATED_True { get; set; }
+        public float SINGLE_VEHICLE_True { get; set; }
+        public float NIGHT_DARK_CONDITION_True { get; set; }
+        public float CITY_MURRAY { get; set; }
+        public float COUNTY_NAME_DAVIS { get; set; }
+        public float COUNTY_NAME_WEBER { get; set; }
+        public float MAIN_ROAD_NAME_Other { get; set; }
+
+        public Tensor<float> AsTensor()
+        {
+            float[] data = new float[]
+            {
+                MILE_POINT, WORK_ZONE_RELATED_True, SINGLE_VEHICLE_True, NIGHT_DARK_CONDITION_True,
+                CITY_MURRAY, COUNTY_NAME_DAVIS, COUNTY_NAME_WEBER, MAIN_ROAD_NAME_Other
+            };
+            int[] dimensions = new int[] { 1, 8 };
+            return new DenseTensor<float>(data, dimensions);
+        }
+
+    }
+}
